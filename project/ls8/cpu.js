cddd41df7c1523c3c8b257fd9fc92525924fcb9e
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-18 15:03:16 -0400

    rebuilt from scratch based on jonbrunt solution

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -1,16 +1,22 @@
-// jonbrunt's awesome solution:
-
+// reserved numeric registers:
 const SP = 0b00000111;
+
+// Jump Instruction booleans:
 let subroutine = false;
 let jump = false;
+
+// Flag input values for CMP:
+const FLAG_EQ = 0; // 0b00000001
+const FLAG_GT = 1; // 0b00000010
+const FLAG_LT = 2; // 0b00000100
 
 class CPU {
   constructor(ram) {
     this.ram = ram;
     this.reg = new Array(8).fill(0);
-    this.reg[SP] = 0xf4;
     this.reg.PC = 0;
-    this.reg.FL = 0b00000000;
+    this.reg.FL = 0;
+    this.reg[SP] = 0xf4;
   }
 
   poke(address, value) {
@@ -20,33 +26,41 @@ class CPU {
   startClock() {
     this.clock = setInterval(() => {
       this.tick();
-    }, 1);
+    }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
   }
 
   stopClock() {
     clearInterval(this.clock);
   }
 
-  error() {
-    console.log('ERROR! Improper argument length');
-    this.stopClock();
-  }
-
   alu(op, regA, regB) {
+    const valA = this.reg[regA];
+    const valB = this.reg[regB];
     switch (op) {
       case 'MUL':
-        this.reg[regA] *= this.reg[regB];
+        this.reg[regA] = valA * valB;
+        break;
+
+      case 'DEC':
+        this.reg[regA] -= 1;
+        break;
+
+      case 'INC':
+        this.reg[regA] += 1;
         break;
       case 'ADD':
-        this.reg[regA] += this.reg[regB];
+        this.reg[regA] = valA + valB;
         break;
-      default:
-        console.log('There was an error in the ALU...command not found');
+      case 'CMP':
+        if (valA > valB) this.reg.FL = FLAG_GT; // 1
+        if (valA < valB) this.reg.FL = FLAG_LT; // 2
+        if (valA == valB) this.reg.FL = FLAG_EQ; // 0
         break;
     }
   }
 
   tick() {
+    // op-codes
     const ADD = 0b10101000;
     const CALL = 0b01001000;
     const CMP = 0b10100000;
@@ -56,20 +70,15 @@ class CPU {
     const JNE = 0b01010010;
     const LDI = 0b10011001;
     const MUL = 0b10101010;
-    const POP = 0b01001100;
     const PRN = 0b01000011;
-    const PUSH = 0b01001101;
     const RET = 0b00001001;
 
     const IR = this.reg.PC;
+
     let operandA = this.ram.read(IR + 1);
     let operandB = this.ram.read(IR + 2);
 
-    // console.log(this.ram.read(IR));
     switch (this.ram.read(IR)) {
-      case ADD:
-        this.alu('ADD', operandA, operandB);
-        break;
       case CALL:
         this.reg[SP] -= 1;
         this.ram.write(this.reg[SP], this.reg.PC + 1);
@@ -77,23 +86,20 @@ class CPU {
         subroutine = true;
         break;
       case CMP:
-        if (this.reg[operandA] === this.reg[operandB]) {
-          this.reg.FL = 0b00000001;
-        } else if (this.reg[operandA] > this.reg[operandB]) {
-          this.reg.FL = 0b00000010;
-        } else this.reg.FL = 0b00000100;
+        this.alu('CMP', operandA, operandB);
         break;
       case HLT:
         this.stopClock();
         break;
+
       case JEQ:
-        if (this.reg.FL === 0b00000001) {
+        if (this.reg.FL === FLAG_EQ) {
           this.reg.PC = this.reg[operandA];
           jump = true;
         }
         break;
       case JNE:
-        if (this.reg.FL !== 0b00000001) {
+        if (this.reg.FL !== FLAG_EQ) {
           this.reg.PC = this.reg[operandA];
           jump = true;
         }
@@ -103,30 +109,23 @@ class CPU {
         jump = true;
         break;
       case LDI:
-        console.log(this.reg);
         this.reg[operandA] = operandB;
         break;
       case MUL:
         this.alu('MUL', operandA, operandB);
         break;
-      case POP:
-        this.reg[operandA] = this.ram.read(this.reg[SP]);
-        this.reg[SP] += 1;
-        break;
       case PRN:
         console.log(this.reg[operandA]);
-        break;
-      case PUSH:
-        this.reg[SP] -= 1;
-        this.ram.write(this.reg[SP], this.reg[operandA]);
         break;
       case RET:
         this.reg.PC = this.ram.read(this.reg[SP]);
         this.reg[SP] += 1;
         break;
       default:
-        console.log(`Error at position: ${IR}, code: ${this.ram.read(IR)}`);
+        let defErr = IR.toString(2);
+        console.error(`Default Error at PC ${this.PC} : ${defErr}`);
         this.stopClock();
+        break;
     }
     if (!subroutine && !jump) {
       this.reg.PC += 1;
